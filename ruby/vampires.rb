a7e3ad8b289ac0707ae9cb6cc852884115a53c4b
Britt Flowers <brittflowermusic@gmail.com>
2017-08-13 11:58:47 -0700

    Add if else statements to vampire detection questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -10,15 +10,25 @@ def vampdetection
   puts "What year were you born?"
     year = gets.chomp
     year = year.to_i
-
+      if age == 2017 - year
+         age == true
+      else age == false
+        puts "Something doesn't add up here..."
+      end
 
   puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
     garlicbread = gets.chomp
-
+      if garlicbread = "Y"
+        garlicbread == true
+      else garlicbread == false
+      end
 
   puts "Would you like to enroll in the company’s health insurance? (Y/N)"
     insurance = gets.chomp
-
+      if insurance = "Y"
+        insurance == true
+      else insurance == false
+      end
 
   if age == true && (garlicbread || insurance )
     puts "Probably not a vampire"
