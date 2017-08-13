9b57e659fdf192b196c48ec15ee1fb2eea033c2e
Britt Flowers <brittflowermusic@gmail.com>
2017-08-13 10:49:34 -0700

    Define a method for a hamster intake form

---
+++ ruby/hamsters.rb
@@ -0,0 +1,33 @@
+
+def hamsterintake
+
+  puts "What's the hamster's name?"
+  hamstername = gets.chomp
+
+  puts "What's the hamster's volume level?"
+  volume = gets.chomp.to_i
+
+  puts "What's the hamster's fur color?"
+  furcolor = gets.chomp
+
+  puts "Is the hamster a good candidate for adoption?"
+  adoption = gets.chomp
+    if adoption.downcase == "yes"
+      adoption = "is"
+    else
+      adoption = "is not"
+    end
+
+  puts "Estimated age?"
+  estimatedage = gets.chomp
+    if estimatedage == ''
+      estimatedage = nil
+    else
+      estimatedage = estimatedage.to_i
+    end
+
+puts "This hamster's name is #{hamstername}, it's volume level is #{volume}, and it's fur color is #{furcolor}. It is estimated to be #{estimatedage} years old and #{adoption} suitable for adoption."
+
+end
+
+hamsterintake
