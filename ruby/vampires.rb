64ffb58dd295d66692d8515e92a9983a4ab3f50b
Britt Flowers <brittflowermusic@gmail.com>
2017-08-14 09:57:33 -0700

    Final attempt

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,6 +1,5 @@
-lovesgarlicbread = true
-wantsinsurance = true
-age = true
+
+
 
 puts "What is your name?"
   name = gets.chomp
@@ -9,41 +8,45 @@ puts "How old are you?"
   age = gets.chomp
   age = age.to_i
 
+
 puts "What year were you born?"
   year = gets.chomp
   year = year.to_i
     if age == 2017 - year
-       age == true
+       age = true
     else
-      age == false
+      age = false
       puts "Something doesn't add up here..."
     end
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
-  garlicbread = gets.chomp
-    if garlicbread = "Y"
-      garlicbread == lovesgarlicbread
-    else garlicbread == false
-      garlic bread == hatesgarlicbread
+  lovesgarlicbread = gets.chomp
+    if lovesgarlicbread == "Y"
+      lovesgarlicbread = true
+    else lovesgarlicbread = false
     end
 
 puts "Would you like to enroll in the company’s health insurance? (Y/N)"
-  insurance = gets.chomp
-    if insurance = "Y"
-      insurance == wantsinsurance
-    else insurance == false
-      insurance == waivesinsurance
+  wantsinsurance = gets.chomp
+    if wantsinsurance == "Y"
+      wantsinsurance = true
+    else wantsinsurance = false
     end
 
-  if age && (lovesgarlicbread || wantsinsurance )
+  if age && (lovesgarlicbread || wantsinsurance)
     puts "Probably not a vampire"
 
-  elsif age == false && !(lovesgarlicbread || wantsinsurance)
-    puts "Probably a vampire!"
+  elsif age = false && (lovesgarlicbread = false || wantsinsurance = false)
+    puts "Probably a vampire"
 
-  elsif age == false && !(lovesgarlicbread && wantsinsurance)
+  elsif age = false && (lovesgarlicbread = false && wantsinsurance = false)
     puts "Almost certainly a vampire!"
+
+  elsif name = "Drake Cula" || "Tu Fang"
+    puts "Definitely a vampire!"
 
   else
     puts "Results inconclusive!"
   end
+
+
