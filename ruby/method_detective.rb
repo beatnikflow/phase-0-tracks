d0f704f700a26f87266cd444cee80465480482f2
Britt Flowers <brittflowermusic@gmail.com>
2017-08-16 11:32:23 -0700

    Uncover enhance and define method

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -13,11 +13,13 @@ end
 
 swapcase
 
+# ....................................... #
 
 
 # "zom".<???>
 # => “zoom”
 
+# ....................................... #
 
  "enhance".center(15)
  => "    enhance    "
@@ -28,27 +30,47 @@ end
 
 enhance
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+# ....................................... #
+
+ "Stop! You’re under arrest!".upcase
+ => "STOP! YOU’RE UNDER ARREST!"
+
+def upcase
+  puts "Stop! You're under arrest!".upcase
+end
+
+# ....................................... #
 
 # "the usual".<???>
 #=> "the usual suspects"
 
+# ....................................... #
+
 # " suspects".<???>
 # => "the usual suspects"
+
+# ....................................... #
 
 # "The case of the disappearing last letter".<???>
 # => "The case of the disappearing last lette"
 
+# ....................................... #
+
 # "The mystery of the missing first letter".<???>
 # => "he mystery of the missing first letter"
+
+# ....................................... #
 
 # "Elementary,    my   dear        Watson!".<???>
 # => "Elementary, my dear Watson!"
 
+# ....................................... #
+
 # "z".<???>
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
+
+# ....................................... #
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4
