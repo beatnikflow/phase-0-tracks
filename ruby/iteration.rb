f0e7583b097642e8e4b47c7d822f9d8500a73412
Britt Flowers <brittflowermusic@gmail.com>
2017-08-16 19:39:22 -0700

    Use .each and map on a hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,26 +9,50 @@ end
 
 animals { |x1, x2| puts "I'm not sure if I want #{x1} or #{x2}" }
 
+# ......................................... #
+
 dog_breeds = ["terrior", "bassett hound", "golden lab", "rottweiler", "corgi"]
 
+puts "This is before the array change"
 p dog_breeds
+
 dog_breeds.each do |dogs|
-  puts dogs
   dogs.next
 end
 
+puts "This is after the array change but before the map change"
 p dog_breeds
 
 dog_breeds.map! do |dogs|
-  puts dogs
-  dog_breeds.rotate
+ puts dogs
+ dogs = dogs + " dog"
 end
 
+puts "This is after the array change with map"
 p dog_breeds
 
+# ........................................... #
+
 pet_types = {
-  cats: "tabby",
-  dogs: "corgi",
-  rabbits: "long ear",
+  cat: "tabby",
+  dog: "corgi",
+  rabbit: "long ear",
   fox: "fennec",
 }
+
+puts "This is before the hash change"
+p pet_types
+
+pet_types.each do |species, type|
+  puts "#{type} is a type of #{species}!"
+end
+
+puts "This is after the hash change"
+p pet_types
+
+pet_types.map do |species, type|
+  puts type
+  type = "orange #{type}"
+end
+
+p pet_types
