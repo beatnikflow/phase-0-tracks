05e0bef25f6948c6130c7916171256620acc8b55
Britt Flowers <brittflowermusic@gmail.com>
2017-08-18 13:07:08 -0700

    Initial commit with pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,16 @@
+
+# Purpose: Write a program that will allow an interior designer to enter the details of a given client: name, age, number of children, decor theme, number of rooms, likes neutral colors, likes bright colors, is a fan of art.
+
+# Steps -
+
+# Prompt the user for information for each question.
+
+# Convert user input to appropriate data type.
+
+# Print the hash onto screen when user has input all information.
+
+# Give user the opportuity to update a key. If user says "none", skip updating. If user enters info to be updated, the program should ask for a new value and update that value.
+
+# Print latest version of hash and exit program.
+
+# ........................................................ #
