30710f04adf6c86c1d4a77a66255911b19bdff15
Britt Flowers <brittflowermusic@gmail.com>
2017-08-19 08:38:29 -0700

    Initial commit with pseudocode

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,8 @@
+# Take a spy's real name: "Sylvia Trench" and write a method that cretes a fake name by doing the following:
+
+#     - Swapping first and last name.
+
+#     - Changing all of the vowels (aeiou) to the next vowel in 'aeiou' (eioua), and all of the consonants to the next consonant in the alphabet.
+
+# -------------------------------------------------------- #
+
