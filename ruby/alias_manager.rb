e66eeb7351600d7ceb302b33a9e987ad03022c07
Britt Flowers <brittflowermusic@gmail.com>
2017-08-19 09:19:33 -0700

    Add basic algorithm

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -6,6 +6,22 @@
 
 # -------------------------------------------------------- #
 
+# ALGORITHM #
+
+# 1. Define a method that will swap words in a user input string.
+
+# 2. Prompt user for name
+
+# 3. Name string words will be swapped by reverse method.
+
+# 4. Define a method that takes reversed name string and changes vowels to the next successive vowel.
+
+# 5. Define a method that takes reversed name string with modified vowels and change consonants to the next successive consonant.
+
+# -------------------------------------------------------- #
+
+
+
 def reverse(string)
     reverse = ""
     index = 0
@@ -21,3 +37,8 @@ spyname = gets.chomp
 
 reverse_sent = reverse(spyname)
 reverse_sent.split.map{|word| reverse(word)}.join(" ")
+
+
+
+def vowel(string)
+
