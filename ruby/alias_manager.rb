9c7f19f4bdade75f701203fc8a9d9b213842d0db
Britt Flowers <brittflowermusic@gmail.com>
2017-08-19 09:06:10 -0700

    Add method that swaps first and last name.

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,4 +1,4 @@
-# Take a spy's real name: "Sylvia Trench" and write a method that cretes a fake name by doing the following:
+# Take a spy's real name and write a method that cretes a fake name by doing the following:
 
 #     - Swapping first and last name.
 
@@ -6,3 +6,18 @@
 
 # -------------------------------------------------------- #
 
+def reverse(string)
+    reverse = ""
+    index = 0
+    while index < string.length
+       reverse = string[index] + reverse
+       index += 1
+    end
+    return reverse
+end
+
+puts "Hello SPY! What is your REAL name?"
+spyname = gets.chomp
+
+reverse_sent = reverse(spyname)
+reverse_sent.split.map{|word| reverse(word)}.join(" ")
