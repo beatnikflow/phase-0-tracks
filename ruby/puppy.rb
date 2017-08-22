9b4c4b41fbf21c46621044ce81e81417b4716ec8
Britt Flowers <brittflowermusic@gmail.com>
2017-08-21 20:42:52 -0700

    Create and design puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# KINGDOM : Animalia
+# ---------------------------------
+
+# CLASS : Mammalia
+# ---------------------------------
+
+# SPECIES : Canis
+# ---------------------------------
+
+# PUPPY AGES : 0 to 9 - 15 months
+# ---------------------------------
+
+# CHARACTERISTICS :
+# ---------------------------------
+# EYES : 2
+# LEGS : 4
+# TAIL : 1
+# COAT : Short, medium, or long-haired
+# COLORS : Varies
+# SIZE : Small to large
+# DIET : Carnivorous
+
+# BEHAVIOR : Small, clumsy, playful, energetic, adorable, messy, plays fetch, learns tricks, eats food, loves treats
+# ---------------------------------
