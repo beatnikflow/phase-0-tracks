30a77e21be4c749c2e434114c7965b6424f66458
Britt Flowers <brittflowermusic@gmail.com>
2017-08-22 15:14:15 -0700

    Add more initialize methods, instance methods and iterate through santa array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,29 @@
+
+
 class Santa
-  def initialize(gender, ethnicity)
+  def initialize(gender, weight, height, ethnicity)
     puts "Initializing Santa instance..."
     @gender = gender
+    @weight = weight
+    @height = height
     @ethnicity = ethnicity
     @age = 0
+  end
+
+  def gender
+    @gender
+  end
+
+  def weight
+    @weight
+  end
+
+  def height
+    @height
+  end
+
+  def ethnicity
+    @ethnicity
   end
 
   def speak
@@ -20,8 +40,27 @@ class Santa
 
 end
 
-santa = Santa.new("female", "african")
-santa.speak
-santa.eat_milk_and_cookies("snickerdoodle")
-p santa.reindeer_ranking
+santas = []
 
+santas << Santa.new("female", "skinny", "short", "Latino")
+santas << Santa.new("bigender", "fat", "5-foot-5", "white")
+santas << Santa.new("male", "pudgy", "dwarf", "Japanese")
+santas << Santa.new("female", "model-like", "tall", "prefer not to say")
+santas << Santa.new("gender fluid", "muscular", "Shaq-height", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A", "N/A", "N/A")
+
+# Other Route:
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+
+santas.each do |santa|
+  puts "I'm a #{santa.gender}, #{santa.weight}, #{santa.height}, #{santa.ethnicity} Santa"
+  santa.speak
+  santa.eat_milk_and_cookies("snickerdoodle")
+  puts "There are #{santas.length} Santa instances in the array"
+  puts "----"
+end
