58fc40fdb751a91ae8e1c298593ed7cc4302e0a3
Britt Flowers <brittflowermusic@gmail.com>
2017-08-23 17:56:20 -0700

    Add call methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -4,8 +4,12 @@ module Shout
   end
 end
 
-module yelling_happily
+module Happy
   def self.yell_happily(words)
     words + "!!!" + " :)"
   end
 end
+
+Shout.yell_angrily("I'm cranky")
+
+Shout.yelling_happily("I'm happy")
