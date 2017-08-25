f7cf4b062c012d1658699bf93e607acdc188313a
Britt Flowers <brittflowermusic@gmail.com>
2017-08-25 11:03:43 -0700

    Create new class with mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,15 +1,34 @@
+# RELEASE 1 #
+
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+# end
+
+# module Happy
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("I'm cranky")
+
+# p Happy.yell_happily("I'm happy")
+
+# --------------------------------------------
+
+# RELEASE 3 #
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_loudly(words)
+    words.upcase + "!!!"
   end
 end
 
-module Happy
-  def self.yell_happily(words)
-    words + "!!!" + " :)"
-  end
+class King_Leonidus
+  include Shout
 end
 
-p Shout.yell_angrily("I'm cranky")
-
-p Happy.yell_happily("I'm happy")
+king = King_Leonidus.new
+p king.yell_loudly("I am Sparta")
