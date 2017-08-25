b188b4ed4e3cc05a44d63494febacc4f0e177d53
Britt Flowers <brittflowermusic@gmail.com>
2017-08-25 11:20:31 -0700

    Add 4 classes to include shout module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -30,5 +30,26 @@ class King_Leonidus
   include Shout
 end
 
+class Napoleon_Dynamite
+  include Shout
+end
+
+class Brick_Tamland
+  include Shout
+end
+
+class William_Wallace
+  include Shout
+end
+
 king = King_Leonidus.new
 p king.yell_loudly("I am Sparta")
+
+napoleon = Napoleon_Dynamite.new
+p napoleon.yell_loudly("Eat the food")
+
+brick = Brick_Tamland.new
+p brick.yell_loudly("loud noises")
+
+william = William_Wallace.new
+p william.yell_loudly("freeeeeeeeeedom")
