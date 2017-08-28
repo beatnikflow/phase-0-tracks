920fbf0e6abfac7ad3656a0485a280fecc94bff0
Britt Flowers <brittflowermusic@gmail.com>
2017-08-27 23:13:15 -0700

    Add if statement to guess_the_word_method

--- ruby/word_game.rb
+++ ruby/word_game.rb
@@ -5,4 +5,18 @@ class WordGame
     @attempts_remaining = game_word.length - 2
   end
 
-end
+  def guess_the_word(word_guess)
+    word_guess.downcase
+    @word.split("").each_with_index do |word_letter, index|
+        if word_guess == word_letter
+          @guesses[index] = word_guess
+          p @guesses.join(" ")
+        end
+    end
+
+    if !@word.include? (word_guess)
+        puts "Nope, guess again..."
+        @attempts_remaining -= 1
+    end
+  end
+
