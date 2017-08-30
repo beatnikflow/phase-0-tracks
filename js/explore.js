6cb396cca6fb59359eb67682c7190603dcd1c45b
Britt Flowers <brittflowermusic@gmail.com>
2017-08-30 12:31:51 -0700

    Create function to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+// Pseudocode a function that takes a string as a parameter and reverses the string. reverse("hello") should return "olleh". Use terms like "for each"
+
+// Create a function called reverse that takes a string
+// function reverse(string)
+// create an empty string.
+// Start a for loop that iterates backwards through the string for each character.
+// Return the new reversed string.
+// Call the function.
+
+// -------------------------------------- //
+
+function reverse(string) {
+  var empty_str = '';
+  for (var reverse_str = string.length - 1; reverse_str >= 0; reverse_str --)
+    empty_str += string[reverse_str];
+  return empty_str;
+}
+
+reverse("hello")
