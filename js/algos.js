4205a2965af75cf3fe8c619ac53c89d710f17cf7
Britt Flowers <brittflowermusic@gmail.com>
2017-09-01 18:50:12 -0700

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Write a function that takes an array of words or phrases and returns the longest word or phrase in the array.
+
+// STEPS:
+// - Declare an array of words
+// - Create a function that accesses the array and uses a length method to find out which word is the longest.
+// - Call the function to return the longest word.
+// - Add driver code that tests the function on a few arrays.
