bf7650920ec7f49bc4b4ff1013013f56fc2e0692
Britt Flowers <brittflowermusic@gmail.com>
2017-09-02 18:02:40 -0700

    Update pseudocode to better reflect the steps to creating the random word function

--- js/algos.js
+++ js/algos.js
@@ -61,11 +61,20 @@ mushroomObject({species: 'Muscaria', genus: 'Amanita'}, {species: 'Pantherina', 
 // STEPS:
 // - Create a function that takes a number in as it's parameter.
 // - Create an empty array.
+// - Create a string of alphabet letters.
 // - Create a for loop to create a new string every time.
-//
+//    - Inside the for loop, use a random number generator method to get a  random string length between 1 and 10.
+//    - Create an empty string
+//    - Nest another loop inside the loop that generates a random character from the alphabet string using the random number generator method as the alphabet's index.
+//    - Set the empty str equal to itself plus the new random character.
+//    - Outside the nested loop but still within the main loop, push the string to the word array.
+// - Add driver code to test the function.
+
+// -----------------------------------------------------------------
 
 function randomWord(num) {
   wordArray = [];
+  var alphaString = "abcdefghijklmnopqrstuvwxyz";
 
   for (var i = 0; i < num; i++) {
     var randomStringLength = Math.floor((Math.random() * 10) + 1);
